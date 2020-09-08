e663298d9827ce2cb53d6800184281cd1beee40e
rafal-sadkiewicz <rafal.sadkiewicz89@gmail.com>
2020-09-08 12:12:22 +0200

    6-obiekt-json

---
+++ 6-obiekt-json/main.js
@@ -0,0 +1,15 @@
+
+
+let jsonPracownicy = 
+{
+    "pracownicy":[
+        {"firstName": "Krystian", "lastName": "Dziopa"}, 
+        {"firstName": "Anna", "lastName": "Szapiel"},
+        {"firstName": "Piotr", "lastName": "Żmuda"}
+    ]
+};
+
+jsonPracownicy.pracownicy.forEach((element, index) => 
+{
+    console.log(`${index + 1}. Imię: ${element.firstName} | Nazwisko: ${element.lastName}`)
+});
